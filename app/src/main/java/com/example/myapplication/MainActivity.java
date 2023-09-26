c751c8d5bb4ff18f3a243c5aa2353ffcb4437878
LeoAlendro171 <118751279+LeoAlendro171@users.noreply.github.com>
2023-09-26 15:55:33 +0200

    mi segundito commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,14 +18,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final Button button = findViewById(R.id.clickMeButton);
+        EditText fieldNumerito = findViewById(R.id.ponNumerito);
+
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                CharSequence text = "Soy el toast!";
+                String numerito = fieldNumerito.getText().toString();
+                CharSequence text = "";
                 int duration = Toast.LENGTH_SHORT;
+                if (numerito.equals("")) {
+                    text = "Introduce un número!";
+                } else {
+                    if (Integer.parseInt(numerito) == randomNumber) {
+                        text = "Has acertado!";
+                    } else if (Integer.parseInt(numerito) > randomNumber) {
+                        text = "Te has pasado!";
+                    } else if (Integer.parseInt(numerito) < randomNumber) {
+                        text = "El número tiene que ser más grande!";
+                    }
+                }
                 Toast toast = Toast.makeText(getApplicationContext(), text, duration);
                 toast.show();
             }
         });
+
     }
 }
